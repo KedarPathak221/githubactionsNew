40c2959446a7bf5898b6f33317d44d9569a1e011
KedarPathak221 <kdar2020@gmail.com>
2025-01-16 14:11:28 +0530

    Completed

--- src/main/java/com/example/githubactions/GithubactionsApplication.java
+++ src/main/java/com/example/githubactions/GithubactionsApplication.java
@@ -2,10 +2,18 @@ package com.example.githubactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubactionsApplication {
 
+	@RequestMapping("/message")
+	public String message()
+	{
+		return "Dockerrrrr is Finalyyyy runningggggg!!!!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GithubactionsApplication.class, args);
 	}
